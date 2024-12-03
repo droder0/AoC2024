1f815ac77f4a5c20d41302d237eaf999425325cd
Daisy Roder <daisyiroder@gmail.com>
2024-12-03 16:02:27 +0000

    Tiny little change

--- Day/Day2.hs
+++ Day/Day2.hs
@@ -9,10 +9,10 @@ part1 :: String -> String
 part1 = show . length . filter (
     \xs -> all (<0) xs && all (>= -3) xs ||
         all (>0) xs && all (<=3) xs)
-    . map ((>>=) tail (zipWith (-)) . map read . words) . lines
+    . map ((tail >>= zipWith (-)) . map read . words) . lines
 
 all1Missing :: [a] -> [[a]]
 all1Missing xs = zipWith (++) (inits xs) (tail $ tails xs)
 
 part2 :: String -> String
-part2 = show . length . filter (any (\xs -> all (<0) xs && all (>= -3) xs || all (>0) xs && all (<=3) xs)) . map (map ((>>=) tail (zipWith (-))) . all1Missing . map read . words) . lines
+part2 = show . length . filter (any (\xs -> all (<0) xs && all (>= -3) xs || all (>0) xs && all (<=3) xs)) . map (map (tail >>= zipWith (-)) . all1Missing . map read . words) . lines
