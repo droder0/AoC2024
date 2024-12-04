6db8698b35703441d11ec123d2fc061514d336a9
Daisy Roder <daisyiroder@gmail.com>
2024-12-04 11:24:29 +0000

    Add Util (from 2022, with `windowed` for day 4)

---
+++ Util.hs
@@ -0,0 +1,21 @@
+module Util where
+import Data.List (tails)
+
+windowed :: Int -> [a] -> [[a]]
+windowed n xs = takeWhile ((==n) . length) $ map (take n) $ tails xs
+
+split :: (a->Bool) -> [a] -> [[a]]
+split p xs = case break p xs of
+    (ys,[]) -> [ys]
+    (ys,z:zs) -> ys : split p zs
+
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf i xs = case splitAt i xs of
+    (ys,[]) -> [ys]
+    (ys,zs) -> ys : chunksOf i zs
+
+stripSuffix :: Eq a => [a] -> [a] -> Maybe [a]
+stripSuffix sfx (x:xs)
+    | sfx == xs = Just [x]
+    | otherwise = fmap (x:) (stripSuffix sfx xs)
+stripSuffix sfx [] = Nothing
